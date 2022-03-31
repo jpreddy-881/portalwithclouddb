fb0f97f11fd66dce231e444c05df378f08829c43
jpreddy-881 <63000534+jpreddy-881@users.noreply.github.com>
2022-03-31 17:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-
-from flask import Flask, render_template, redirect, url_for, session, g
-from flask_session import Session
-from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, send_file, request, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from pyasn1.type.univ import Null
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
@@ -17,38 +13,27 @@ from flask_login import (
     logout_user,
     current_user,
 )
-import pathlib
-import requests
-from flask import Flask, session, abort, redirect, request
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
-
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-import threading
-import os
 import shutil
-from flask import Flask, redirect, url_for, request, flash
-from flask import render_template
-from flask import send_file
-import csv
-from urllib.parse import quote 
-from flask_migrate import Migrate
+import os
+ 
+global name
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Thisissupposedtobesecret!"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://jpr@avistosdb01:%s@avistosdb01.mysql.database.azure.com/avistos" % quote('Prakash.881')
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "sqlite:///C:\\Users\\damodar.pulimamidi\\Desktop\\28_2\\database.db"
 
 bootstrap = Bootstrap(app)
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -57,11 +42,10 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(80))
     role = db.Column(db.String(100))
     client = db.Column(db.String(100))
-  
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    normal = str(user_id)
     return User.query.get(int(user_id))
 
 
@@ -76,7 +60,6 @@ class LoginForm(FlaskForm):
     client = StringField("client ", validators=[InputRequired(), Length(min=4, max=25)])
     remember = BooleanField("remember me")
 
-    
 
 class RegisterForm(FlaskForm):
     email = StringField(
@@ -94,27 +77,28 @@ class RegisterForm(FlaskForm):
     remember = BooleanField("remember me")
 
 
-
 @app.route("/")
 def index1():
     return render_template("index1.html")
 
 
-@app.route("/login1", methods=["GET", "POST"])
-def login1():
-
+@app.route("/login", methods=["GET", "POST"])
+def login():
     form = LoginForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
+            # if user.password == form.password.data:
+            #     return redirect(url_for('dashboard'))
 
-            if user.password == (form.password.data):
+            if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
+                return redirect(url_for("dashboard"))
 
-                return redirect(url_for("dashboard1"))
+        return "<h1>Invalid username or password</h1>"
 
-    return render_template("login1.html", form=form)
+    return render_template("login.html", form=form)
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -122,14 +106,13 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        #hashed_password = generate_password_hash(form.password.data, method="sha256")
+        hashed_password = generate_password_hash(form.password.data, method="sha256")
         new_user = User(
             username=form.username.data,
             email=form.email.data,
             role=form.role.data,
             client=form.client.data,
-            password=form.password.data
-            #hashed_password,
+            password=hashed_password,
         )
         db.session.add(new_user)
         db.session.commit()
@@ -139,16 +122,19 @@ def signup():
     return render_template("signup.html", form=form)
 
 
-@app.route("/dashboard1")
+@app.route("/dashboard")
 @login_required
-def dashboard1():
-    return render_template("dashboard1.html", name=current_user.username,  datetime=str(datetime.now()) )
-
+def dashboard():
+    name=current_user.username
+    return render_template("dashboard.html", name=current_user.username)
 
 @app.route("/logout")
 @login_required
-def logout():
-    session["user"] = None
+def logout():   
+    zcwd5 = os.getcwd()  
+    zuser_dir9 = os.path.join(zcwd5,current_user.username)
+    shutil.rmtree(zuser_dir9)
+
     logout_user()
     return redirect(url_for("index1"))
 
@@ -156,87 +142,176 @@ def logout():
 admin = Admin(app)
 admin.add_view(ModelView(User, db.session))
 
-########################## Google Flow Started  #######################################
+#######################Tool Started ##############################
 
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-GOOGLE_CLIENT_ID = "919473826120-rp26v9k64c5d7ourmgqj7cmq2b5pfp28.apps.googleusercontent.com"
-
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+@app.route("/tool", methods=["GET", "POST"])
+def index():
     
-    redirect_uri="http://127.0.0.1:5000/callback"
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No files selected")
+            return redirect("/tool")
+        try:
+            pass   
+           
+        except:
+            pass
+        os.mkdir(current_user.username)
+        files = request.files.getlist("file")
+        for f in files:
+            f.save(os.path.join(current_user.username, f.filename))
+        cwd = os.getcwd()
+        user_dir1 = os.path.join(cwd,current_user.username)
+        global user_dir
+        user_dir = os.path.basename(user_dir1)
+        app.config["IMAGES"] = user_dir
+        for (dirpath, dirnames, filenames) in os.walk(app.config["IMAGES"]):
     
-)
+            files = filenames
+            break
+        app.config["FILES"] = files
+        app.config["HEAD"] = 0
+        return redirect("/tagger", code=302)
+    else:
+        return render_template("index.html",myname = current_user.username)
 
-
-def login_is_required(function):
-    def wrapper(*args, **kwargs):
-        if "google_id" not in session:
-            return abort(401)  # Authorization required
-        else:
-            return function()
-
-    return wrapper
-
-
-@app.route("/login")
-def login():
-    authorization_url, state = flow.authorization_url()
-    session["state"] = state
-    return redirect(authorization_url)
-
-
-@app.route("/callback")
-def callback():
-    flow.fetch_token(authorization_response=request.url)
-
-    if not session["state"] == request.args["state"]:
-        abort(500)  # State does not match!
-
-    credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
-
-    id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
-        request=token_request,
-        audience=GOOGLE_CLIENT_ID
+@app.route("/tagger")
+def tagger():
+    if app.config["HEAD"] == len(app.config["FILES"]):
+        return redirect(url_for("final"))
+    
+    directory = app.config["IMAGES"]
+    image = app.config["FILES"][app.config["HEAD"]]
+    labels = app.config["LABELS"]
+    not_end = not (app.config["HEAD"] == len(app.config["FILES"]) - 1)
+    print(not_end)
+    return render_template(
+        "tagger.html",
+        not_end=not_end,
+        directory=directory,
+        image=image,
+        labels=labels,
+        head=app.config["HEAD"] + 1,
+        len=len(app.config["FILES"]),
     )
 
-    session["google_id"] = id_info.get("sub")
-    session["name"] = id_info.get("name")
-    
-    gid =  session["google_id"]
-    gname =  session["name"]
-        
-    return redirect("/dashboard")
- 
+
+@app.route("/next")
+def next():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] + 1
+    with open(app.config["OUT"], "a") as f:
+        for label in app.config["LABELS"]:
+            f.write(
+                image
+                + ","
+                + label["id"]
+                + ","
+                + label["name"]
+                + ","
+                + str(round(float(label["xMin"])))
+                + ","
+                + str(round(float(label["xMax"])))
+                + ","
+                + str(round(float(label["yMin"])))
+                + ","
+                + str(round(float(label["yMax"])))
+                + "\n"
+            )
+    app.config["LABELS"] = []
+    return redirect(url_for("tagger"))
 
 
-@app.route("/logout1")
-def logout1():
-    session.clear()
-    return redirect("/")
-   
+@app.route("/prev")
+def prev():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] - 1
+    with open(app.config["OUT"], "a") as f:
+        for label in app.config["LABELS"]:
+            f.write(
+                image
+                + ","
+                + label["id"]
+                + ","
+                + label["name"]
+                + ","
+                + str(round(float(label["xMin"])))
+                + ","
+                + str(round(float(label["xMax"])))
+                + ","
+                + str(round(float(label["yMin"])))
+                + ","
+                + str(round(float(label["yMax"])))
+                + "\n"
+            )
+    app.config["LABELS"] = []
+    return redirect(url_for("tagger"))
 
 
-@app.route("/")
-def index():
-    return "Hello World <a href='/login'><button>Login</button></a>"
+@app.route("/final")
+def final():
+    return render_template("final.html")
 
-@app.route("/dashboard")
-@login_is_required
-def protected_area():
-    return render_template("dashboard.html",datetime=str(datetime.now()),name=session["name"] )
-        
 
-########################### Endof Google flow #################################
+@app.route("/add/<id>")
+def add(id):
+    xMin = request.args.get("xMin")
+    xMax = request.args.get("xMax")
+    yMin = request.args.get("yMin")
+    yMax = request.args.get("yMax")
+    app.config["LABELS"].append(
+        {"id": id, "name": "", "xMin": xMin, "xMax": xMax, "yMin": yMin, "yMax": yMax}
+    )
+    return redirect(url_for("tagger"))
+
+
+@app.route("/remove/<id>")
+def remove(id):
+    index = int(id) - 1
+    del app.config["LABELS"][index]
+    for label in app.config["LABELS"][index:]:
+        label["id"] = str(int(label["id"]) - 1)
+    return redirect(url_for("tagger"))
+
+
+@app.route("/label/<id>")
+def label(id):
+    name = request.args.get("name")
+    app.config["LABELS"][int(id) - 1]["name"] = name
+    return redirect(url_for("tagger"))
+
+
+@app.route("/image/<f>")
+def images(f):
+    images = app.config["IMAGES"]
+    return send_file(images + "/" + f)
+
+
+@app.route("/download")
+def download():
+    zcwd = os.getcwd()
+    zuser_dir1 = os.path.join(zcwd,current_user.username)
+    zuser_dir2 = os.path.join(zuser_dir1, "annotations.csv")
+    shutil.copyfile("out.csv", zuser_dir2)
+    shutil.make_archive("final", "zip", zuser_dir1)
+    return send_file(
+        "final.zip",
+        mimetype="text/csv",
+        attachment_filename="final.zip",
+        as_attachment=True,
+    )
+
+@app.route('/repo')
+def repo():
+    pass
+    return redirect(url_for("index1"))
+
 
 if __name__ == "__main__":
-    #app.run(debug=True)
+    app.config["LABELS"] = []
+    app.config["HEAD"] = 0
+    app.config["OUT"] = "out.csv"
+    with open("out.csv", "w") as f:
+        f.write("image,id,name,xMin,xMax,yMin,yMax\n")
+    # app.run(debug="True")
     app.run(debug="True", host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
